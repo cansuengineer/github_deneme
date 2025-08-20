ea1413cbac66f5da380ac9636b4234c9e906bd77
Cansu Sarp <cansu.uzunsimsek.1@gmail.com>
2025-08-20 23:06:49 +0300

    Initial Commit

---
+++ github_deneme/github_denemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_denemeApp.swift
+//  github_deneme
+//
+//  Created by Cansu Uzunsimsek on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct github_denemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
